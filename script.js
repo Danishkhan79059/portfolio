f0ef7fa8ab9df99724453de126568884e129c93f
Mompa786 <123612696+Mompa786@users.noreply.github.com>
2023-01-28 10:20:33 +0530

    Add files via upload
    i am a lerner and i made a portfolio by itself

---
+++ script.js
@@ -0,0 +1,13 @@
+console.log("don")
+document.querySelector('.close').style.display = 'none';
+document.querySelector('.hamburger').addEventListener("click", ()=>{
+    document.querySelector('.sidebar').classList.toggle('sidebarGo');
+    if(document.querySelector('.sidebar').classList.contains('.sidebarGo')){
+        document.querySelector('.ham').style.display ='inline'
+        document.querySelector('.close').style.display ='none'
+    }
+    else{
+        document.querySelector('.ham').style.display ='none'
+        document.querySelector('.close').style.display ='inline'
+    }
+})
